7c2aaa089c90ae3643eb4a952646c72f7a0ff69f
MatthewVicente <mathewvicente@gmail.com>
2020-03-06 15:45:42 -0300

    Use body parser and create login route to render simple form

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -6,3 +6,10 @@ exports.router = router;
 router.get('/', function (req, res) {
     res.send('hi there');
 });
+router.get('/login', function (req, res) {
+    res.send("\n\t\t<form method=\"POST\">\n\t\t\t<div>\n\t\t\t\t<label>Email</label>\n\t\t\t\t<input name=\"email\"/>\n\t\t\t</div>\n\t\t\t<div>\n\t\t\t\t<label>Password</label>\n\t\t\t\t<input name=\"password\" type=\"password\"/>\n\t\t\t</div>\n\t\t\t<button>Submit</button>\n\t\t</form>\n\t");
+});
+router.post('/login', function (req, res) {
+    var _a = req.body, email = _a.email, password = _a.password;
+    res.send(email + password);
+});
